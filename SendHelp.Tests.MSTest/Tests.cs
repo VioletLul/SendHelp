b87b7eead8f0feb5463b381a025931075ba76698
Jason Roesser <j.roesser-ecdl@t-online.de>
2023-11-17 14:12:12 +0100

    Added Empty project

---
+++ SendHelp.Tests.MSTest/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SendHelp.Tests.MSTest;
+
+// TODO: Add appropriate unit tests.
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ SendHelp/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Prism.Mvvm;
+
+namespace SendHelp.ViewModels;
+
+public class MainViewModel : BindableBase
+{
+    public MainViewModel()
+    {
+    }
+}
